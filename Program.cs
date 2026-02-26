05d5eac764e540742ca96c47bed41298fd7efb3c
Parker Arnett <362581936@GoTVDSB.ca>
2026-02-26 13:19:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace While_Loop_Programming_Assignments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+        }
+        public static void Part1()
+        {
+            //Simple casino game
+            Random gen= new Random();
+
+            int score,
+            num = gen.Next(1, 3);
+            bool done = false;
+            score = 3;
+
+
+
+
+
+
+
+        }
+    }
+}
